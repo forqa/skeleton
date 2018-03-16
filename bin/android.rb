818b0bddcc1387c6596797c3db5ce339c5ec9a71
Dmitry Shcherbakov <dshcherbakov@smartlabs.tv>
2018-03-16 14:01:18 +0300

    reafctor

--- bin/android.rb
+++ bin/android.rb
@@ -1,5 +1,10 @@
-class Android
-  @@TIMESTAMP = (Time.now.to_f * 1000).to_i
+class Android < Base
+  @@RESOURCE_ID = "resource-id"
+  @@CONTENT_DESC = "content-desc"
+  @@TEXT = "text"
+  @@ID = "id"
+  @@XPATH = "xpath"
+  @@CLASS = "class"
 
   attr_accessor :platform, :udid, :bundle_id, :ios_sim, :dir
 
@@ -11,47 +16,43 @@ class Android
 
   def skeletoner
     dump = %x(adb -s #{@udid} shell uiautomator dump | egrep -o '/.*?xml')
-    page_source = %x(adb -s #{@udid} shell cat #{dump})
-    get_elements_locators(page_source)  
+    page_source = %x(adb -s #{@udid} shell cat #{dump})  
+    create_page_objects(page_source)  
   end
 
 
   private
 
-  def get_elements_locators(page_source)
-    i = 0
-    p page_source
+  def create_locator_by_resouce_id(element)
+    name = element[@@RESOURCE_ID]
+    name.slice!(/.*id\//)
+    add_locator_to_page_objects(name, @@ID, element[@@RESOURCE_ID])
+  end
+
+  def create_locator_by_content_desc(element)
+    name = element[@@CONTENT_DESC]
+    name.slice!(/.*id\//)
+    locator = "//#{element[@@CLASS]}[@#{@@CONTENT_DESC}='#{element[@@CONTENT_DESC]}']"
+    add_locator_to_page_objects(name, @@XPATH, locator)
+  end
+
+  def create_locator_by_text(element)
+    name = element[@@CLASS]
+    name.slice!(/.*\./)
+    locator = "//#{element[@@CLASS]}[@#{@@TEXT}='#{element[@@TEXT]}'"
+    add_locator_to_page_objects(name, @@XPATH, locator)
+  end
+
+  def create_locator(element)
+    !element[@@RESOURCE_ID].empty? && create_locator_by_resouce_id(element) and
+    !element[@@CONTENT_DESC].empty? && create_locator_by_content_desc(element) and
+    !element[@@TEXT].empty? && create_locator_by_text(element)
+  end
+
+  def create_page_objects(page_source)
     doc = Nokogiri::HTML.parse(page_source)
     doc.css('node').map {|element|
-    attribute = element["resource-id"]
-    if  attribute.empty? 
-      attribute = element["content-desc"]
-      if attribute.empty?
-        attribute = element["text"]
-        if not attribute.empty?
-          elem_class = element["class"]
-          locator = "//#{elem_class}[@text='#{attribute}]'"
-          name = elem_class
-          name.slice!(/.*\./)
-          name += i.to_s
-          i += 1
-          create_page_objects(name, "xpath", locator)
-        end
-      else 
-        name = attribute
-        name.slice!(/.*\./)
-        name += i.to_s
-        i += 1
-        locator = "//#{elem_class}[@content-desc='#{attribute}]'"
-        create_page_objects(name, "xpath", locator)
-      end
-    else
-      p "ResoureId #{attribute}"
-      name = attribute
-      name.slice!(/.*id\//)
-      p "NAME #{name}"
-      create_page_objects(attribute, "id", attribute)
-    end
+      create_locator(element)
     }
   end
 
@@ -59,12 +60,11 @@ class Android
     "By #{name}() {\n\treturn MobileBy.#{locator_type}(\"#{value}\");\n}\n\n"
   end
 
-  def create_page_objects(name, locator_type, value)
+  def add_locator_to_page_objects(name, locator_type, value)
     File.open("#{@dir}/#{@platform}_#{@@TIMESTAMP}.java", 'a') do |f|
       f.write(get_java_method(name, locator_type, value))
     end
   end
-
-
+  
 
 end
