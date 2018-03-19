997c161c1ad936a1f9de0681c044b2a02606bad1
Alexey Alter-Pesotskiy <33gri@bk.ru>
2018-03-19 08:52:14 +0300

    upgrade camel and snake parsers

--- bin/base.rb
+++ bin/base.rb
@@ -6,32 +6,30 @@ class Base
   TIMESTAMP = (Time.now.to_f * 1000).to_i
 
   def snake_style(method_name)
-    method_name[0] = method_name[0].downcase
     method_name.each_char.with_index do |char, char_i|
       method_name[char_i] =
-        if char == ' ' || char == '-'
+        if /[ -!$%^&*()+|~=`{}\[\]:";'<>?,.\/]/.match(method_name[char_i])
           '_'
         elsif /[A-Z]/.match(method_name[char_i])
           if method_name[char_i - 1] != '_'
-            '_' + method_name[char_i].downcase
+            "_#{method_name[char_i]}"
           else
-            method_name[char_i].downcase
+            method_name[char_i]
           end
         else
           method_name[char_i]
         end
     end
-    method_name.squeeze('_')
+    method_name.squeeze('_').downcase
   end
 
   def camel_style(method_name)
     space_i = 0
     method_name[0] = method_name[0].downcase
     method_name.each_char.with_index do |char, char_i|
-      if char == ' ' || char == '_' || char == '-'
+      if /[ -!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.match(char)
         method_name[char_i - space_i] = ''
-        method_name[char_i - space_i] =
-          method_name[char_i - space_i].capitalize
+        method_name[char_i - space_i] = method_name[char_i - space_i].capitalize
         space_i += 1
       end
     end
