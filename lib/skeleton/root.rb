d68d90e56535020bf86530c1541302a48b6c3b07
alter-al <33gri@bk.ru>
2018-07-27 15:47:17 +0300

    fix root_dir path

--- lib/skeleton/root.rb
+++ lib/skeleton/root.rb
@@ -1,7 +1,7 @@
 require_relative 'version'
 
 class Base
-    ROOT_DIR = File.expand_path('..', Dir.pwd)
-    #  ROOT_DIR = "#{`gem environment gemdir`.strip}" \
-    #              "/gems/#{Skeleton::GEM_NAME}-#{Skeleton::VERSION}"
+   # ROOT_DIR = File.expand_path('..', Dir.pwd)
+   ROOT_DIR = "#{`gem environment gemdir`.strip}" \
+               "/gems/#{Skeleton::GEM_NAME}-#{Skeleton::VERSION}"
 end
